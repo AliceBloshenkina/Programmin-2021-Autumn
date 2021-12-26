cb971c1bc66ebb5d4209d3001b41c86938f49cb0
Alice Bloshenkina <48186115+AliceBloshenkina@users.noreply.github.com>
2021-12-27 02:25:39 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-
 #include "Cashe.h"
+#include <windows.h>
 
 struct Sheep{
     string name;
@@ -15,41 +15,84 @@ int HashFunction(I key, size_t size){
 
 int main(){
     int (*function)(string, size_t) = HashFunction;
-//    HashTable<string, struct Sheep> hashtable(function, 4);
+    int size = 4;
 
-    struct Sheep Enn, Kate, Dolly, Mark;
+    // ТЕСТЫ ДЛЯ ХЭШ-ТАБЛИЦЫ
+    HashTable<string, struct Sheep> hashtable(function, size);
+
+    struct Sheep Enn, Kate, Molly, Mark;
     Enn.name = "Enn Unisted";
     Kate.name = "Kate Markov";
-    Dolly.name = "Molly Green";
+    Molly.name = "Molly Green";
     Mark.name = "Mark Arkined";
 
     Enn.age = 13;
     Kate.age = 10;
-    Dolly.age = 25;
+    Molly.age = 25;
     Mark.age = 15;
 
-    cout << Enn.name << " - " << HashFunction(Enn.name, 4) << endl;
-    cout << Kate.name << " - " << HashFunction(Kate.name, 4) << endl;
-    cout << Dolly.name << " - " << HashFunction(Dolly.name, 4) << endl;
-    cout << Mark.name << " - " << HashFunction(Mark.name, 4) << endl << endl;
+    cout << Enn.name << " - " << HashFunction(Enn.name, size) << endl;
+    cout << Kate.name << " - " << HashFunction(Kate.name, size) << endl;
+    cout << Molly.name << " - " << HashFunction(Molly.name, size) << endl;
+    cout << Mark.name << " - " << HashFunction(Mark.name, size) << endl << endl;
 
-//    hashtable.append(Enn.name, Enn);
-//    hashtable.append(Mark.name, Mark);
-//    hashtable.append(Dolly.name, Dolly);
-//    hashtable.append(Kate.name, Kate);
 
-//    hashtable.remove(Kate.name);
+    hashtable.append(Enn.name, Enn);
+    hashtable.append(Molly.name, Molly);
+    hashtable.append(Mark.name, Mark);
+    hashtable.append(Kate.name, Kate);
 
-//    hashtable.append(Kate.name);
-//    hashtable.printvect();
+    cout << "Removing :" << endl;
 
-    Cashe<string, struct Sheep> cash(4, function, 40);
+    cout << "WAS - ";
+    hashtable.printa(1);
+    if (hashtable.search(Enn.name)){
+        hashtable.remove(Enn.name);
+        cout << "Now - ";
+        hashtable.printa(1);
+        if (!hashtable.search(Enn.name))
+            cout << "A first element - correct" << endl << endl;
+    }
+
+    cout << "WAS - ";
+    hashtable.printa(1);
+    if (hashtable.search(Mark.name)){
+        hashtable.remove(Mark.name);
+        cout << "Now - ";
+        hashtable.printa(1);
+        if (!hashtable.search(Mark.name))
+            cout << "A last element -  correct" << endl << endl;
+    }
+
+    hashtable.append(Enn.name, Enn);
+    hashtable.append(Mark.name, Mark);
+    cout << "WAS - ";
+    hashtable.printa(1);
+    if (hashtable.search(Enn.name)){
+        hashtable.remove(Enn.name);
+        cout << "Now - ";
+        hashtable.printa(1);
+        if (!hashtable.search(Enn.name))
+            cout << "A middle element - correct" << endl << endl;
+    }
+
+
+    struct Sheep one = hashtable.get(Kate.name);
+
+    cout << "Get is work - " << one.name << " " << one.age << endl << endl;
+
+
+    cout << "CASHE: " << endl;
+    Cashe<string, struct Sheep> cash(4, function, 20);
     cash.push(Enn.name, Enn);
-    if(cash.search(Enn.name))
-        cout << "OK";
-
-//    cash.remove();
-
-
+    if (cash.search(Enn.name))
+        cout << "Append is work" << endl;
+    Sleep(10);
+    cash.push(Mark.name, Mark);
+    Sleep(10);
+    cash.push(Molly.name, Molly);
+    Sleep(10);
+    if (!cash.search(Enn.name) && cash.search(Mark.name))
+        cout << "Time to delete is working";
     return (0);
 }
